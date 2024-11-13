4b287de106a190c9d8613400c8ff0b4b14840cc5
Nadia <nlnadialigia@gmail.com>
2024-11-13 09:56:16 -0300

    criação e estilização do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,23 @@
+import {MapPin, ShoppingCart} from "phosphor-react";
+import Logo from "../../assets/logo.svg";
+import {HeaderCart, HeaderContainer, HeaderInfo, HeaderLocation} from "./styles";
+
 export function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <HeaderContainer>
+      <a href="/">
+        <img src={Logo} alt="" />
+      </a>
+
+      <HeaderInfo>
+        <HeaderLocation>
+          <MapPin size={22} weight="fill" color="#8047F8" />
+          <span>Porto Alegre, RS</span>
+        </HeaderLocation>
+        <HeaderCart href="/checkout">
+          <ShoppingCart size={22} weight="fill" color="#C47F17" />
+        </HeaderCart>
+      </HeaderInfo>
+    </HeaderContainer>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  height: 6.5rem;
+  padding: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    height:40;
+    width:85;
+  }
+`;
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  position: relative;
+`;
+
+export const HeaderLocation = styled.button`
+  background: ${props => props.theme['purpleLight']};
+  color: ${props => props.theme['purpleDark']};
+  border: none;
+  border-radius: 6px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+
+  span {
+    font-size: 0.875rem;
+  }
+`;
+
+
+
+export const HeaderCart = styled.a`
+  background: ${props => props.theme['yellowLight']};
+  color: ${props => props.theme['yellowDark']};
+  border: none;
+  border-radius: 6px;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::after {
+    content: '';
+    display: none;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    top: -0.5rem;
+    right: -0.5rem;
+
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 50%;
+    background: ${props => props.theme['yellowDark']};
+    color: ${props => props.theme['white']};
+    font-size: 0.75rem;
+    font-weight: 700;
+  }
+`;
